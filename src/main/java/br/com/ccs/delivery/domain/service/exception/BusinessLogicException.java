0191109317bf2e745cd7f3b8cbfb8e1d23c2f142
Cleber Souza <ccs1201@gmail.com>
2022-06-02 18:40:46 -0300

    Implementado classe BusinessLogicException para representar as exceções de regra de negocio.

---
+++ src/main/java/br/com/ccs/delivery/domain/service/exception/BusinessLogicException.java
@@ -0,0 +1,17 @@
+package br.com.ccs.delivery.domain.service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BusinessLogicException extends RuntimeException{
+
+    public BusinessLogicException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public BusinessLogicException(String message) {
+        super(message);
+
+    }
+}
