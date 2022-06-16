ec03c7eab937395eef7c4688744cc3c416bdfdbb
Cleber Souza <ccs1201@gmail.com>
2022-06-16 12:58:54 -0300

    Projeto alterado para usar TimeZone UTC (UTC-0)
    Alterada a url de conexão para utilizar serverTimezone=UTC
    
    Alteradas a classes Restaurante e RestauranteMixin para utilizar OffSetDateTime ao invés de LocalDateTime
    
    Adicionada a configuração de timeZone no DeliveryApiApplication (TimeZone.setDefault(TimeZone.getTimeZone("UTC"));)

--- src/main/java/br/com/ccs/delivery/DeliveryApiApplication.java
+++ src/main/java/br/com/ccs/delivery/DeliveryApiApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaRepositories(repositoryBaseClass = CustomJpaRepositoryImpl.class)
 public class DeliveryApiApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 		SpringApplication.run(DeliveryApiApplication.class, args);
 	}
-
 }
