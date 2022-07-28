e8c0f5470601686e0701699a561b580b4f7b9ca0
Cleber Souza <ccs1201@gmail.com>
2022-07-28 11:56:19 -0300

    Adicionado documentação do método PedidoConfirmadoListener.onConfirmarPedido

--- src/main/java/br/com/ccs/delivery/domain/listener/PedidoConfirmadoListener.java
+++ src/main/java/br/com/ccs/delivery/domain/listener/PedidoConfirmadoListener.java
@@ -12,9 +12,22 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class PedidoConfirmadoListener {
     private MailService mailService;
 
-    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMPLETION)
+    /**
+     * Envia um e-mail ao cliente durante um evento
+     * de confirmação de pedido.
+     *<p>
+     * A anotação <b>{@code @TransactionalEventListener}</b> com <b>phase</b>
+     * <b>{@code TransactionPhase.BEFORE_COMMIT} </b>faz com que o e-mail seja enviado
+     * antes do commit no banco de dados e caso seja lançada alguma
+     * exceção durante a tentativa de envio do e-mail, será efetuado
+     * o rollback da transação no banco de dados.
+     *</p>
+     * @param event o {@link PedidoConfirmadoEvent} que carrega o pedido gerador do evento
+     * <p><b>Return</b> void</p>
+     * */
+    @TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
     //@EventListener
-    public void onConfirmPedido(PedidoConfirmadoEvent event) {
+    public void onConfirmarPedido(PedidoConfirmadoEvent event) {
 
         mailService.send(event.getPedido());
 
