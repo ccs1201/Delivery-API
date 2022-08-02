8107a7bcd77cf705891cd2070be4eb0798dd29fa
Cleber Souza <ccs1201@gmail.com>
2022-08-02 18:14:20 -0300

    Implementado ShallowEtagFilterConfig para configurar o suporte a Etags.
    Etags calculam um hash para a reposta que será enviada, no response header para o cliente.
    
    Uma vez que o cliente possui um objeto no cache que possui uma etag esta é enviada no request header como if-Nome-Match: "hash" com o ShallowEtagHeaderFilter habilitado o backend (neste caso o servlet filter) verifica o hash enviado na request header com o hash gerando para a response caso eles sejam iguais o servidor retorna status 304 Not Modified sem corpo para que o cliente utilize a versão em cache.
    
    Caso os hashes sejam diferentes o servidor envia uma reposta completa normalmente.
    
    Isto poupa recursos de rede (trafego) especialmente para clientes mobile.

---
+++ src/main/java/br/com/ccs/delivery/core/configurations/ShallowEtagFilterConfig.java
@@ -0,0 +1,16 @@
+package br.com.ccs.delivery.core.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class ShallowEtagFilterConfig {
+
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
+    }
+}
