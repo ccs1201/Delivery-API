d1b3a31a6da9e8253e4880aa836fca2719e20f01
Cleber Souza <ccs1201@gmail.com>
2022-11-21 16:51:38 -0300

    20. Evoluindo e versionando a API
    20.12 - Definindo a versão padrão da API quando o Media Type não é informado

--- src/main/java/br/com/ccs/delivery/core/configurations/WebConfig.java
+++ src/main/java/br/com/ccs/delivery/core/configurations/WebConfig.java
@@ -1,6 +1,7 @@
 package br.com.ccs.delivery.core.configurations;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,5 +17,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("*");
                 //.allowedOrigins("http://localhost:8081/")
                 //.maxAge()
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(ApiMediaTypes.V2_APPLICATION_JSON);
     }
 }
