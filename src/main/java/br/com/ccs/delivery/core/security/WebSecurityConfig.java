c30c32d38361dc00a298e1f571b6117f5904727c
Cleber Souza <ccs1201@gmail.com>
2023-06-12 21:52:06 -0300

    #Modulo 23.6 Configurando o Resource Server para JWT assinado com chave simétrica

--- src/main/java/br/com/ccs/delivery/core/security/WebSecurityConfig.java
+++ src/main/java/br/com/ccs/delivery/core/security/WebSecurityConfig.java
@@ -1,13 +1,20 @@
 package br.com.ccs.delivery.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final String SECRET = "QS1zyuuDIHklis01*n15%PLVLaR64@Xj";
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
@@ -16,6 +23,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .cors().and()
-                .oauth2ResourceServer().opaqueToken();
+                .oauth2ResourceServer().jwt();
+//                .oauth2ResourceServer().opaqueToken();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        var secretkey = new SecretKeySpec(SECRET.getBytes(), "HmacSHA256");
+        return NimbusJwtDecoder.withSecretKey(secretkey).build();
     }
 }
